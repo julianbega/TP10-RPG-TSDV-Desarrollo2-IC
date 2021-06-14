269934ce60582ac52fc7d174ce8854621bb74e47
Julian Bega <julian.begamilrad@gmail.com>
2021-06-14 00:27:03 -0300

    EnemyMovment

---
+++ TP10-RPG/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    public float speed = 1;
+    GameObject target;
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.transform.position, this.transform.position);
+
+        if (distance < lookRadius)
+        {
+            agent.SetDestination(target.transform.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+}
