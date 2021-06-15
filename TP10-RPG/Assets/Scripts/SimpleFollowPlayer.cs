78bdf357cb23568c230857eb6fadf2e8f9190dd1
Adrian <Owinmowe@gmail.com>
2021-06-15 04:08:04 -0300

    Added a simple camera follow for the player.

---
+++ TP10-RPG/Assets/Scripts/SimpleFollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] float distanceY = 10;
+    [SerializeField] float distanceZ = 15;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + distanceY, player.transform.position.z - distanceZ);
+        transform.LookAt(player.transform);
+    }
+
+}
